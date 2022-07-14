a63b53d9b0077b360cbf85f604f130ed4b745a63
Ritesh <riteshkokam@gmail.com>
2022-07-14 23:01:34 +0530

    initial commit

---
+++ public/receiver.js
@@ -0,0 +1,71 @@
+(function () {
+  let senderID;
+  const socket = io();
+
+  function generateID() {
+    return `${Math.trunc(Math.random() * 999)}-${Math.trunc(
+      Math.random() * 999
+    )}-${Math.trunc(Math.random() * 999)}`;
+  }
+
+  document
+    .querySelector("#reciver-start-con-btn")
+    .addEventListener("click", function () {
+      senderID = document.querySelector("#join-id").value;
+      if (senderID.length == 0) {
+        return;
+      }
+      let joinID = generateID();
+
+      socket.emit("reciver-join", {
+        uid: joinID,
+        sender_uid: senderID,
+      });
+    });
+
+  socket.on("init", function () {
+    receiverID = uid;
+    document.querySelector(".join-screen").classList.remove("active");
+    document.querySelector(".fs-screen").classList.add("active");
+  });
+
+  let filaeShare = {};
+
+  socket.on("fs-meta", function (metadata) {
+    fileShare.maetadata = metadata;
+    fileShare.transmitted = 0;
+    fileShare.buffer = [];
+
+    let el = document.createElement("div");
+    el.classList.add("item");
+    el.innerHTML = `
+        <div class="progress">0%</div>
+        <div class="filename">${metadata.filename}</div>
+    `;
+
+    document.querySelector(".files-list").appendChild(el);
+
+    fileShare.progress_node = el.querySelector(".progress");
+
+    socket.emit("fs-start", {
+      uid: senderID,
+    });
+  });
+
+  socket.on("fs-share", function (buffer) {
+    fileShare.buffer.push(buffer);
+    fileShare.transmitted += buffer.byteLength;
+    fileShare.progress_node.innerText =
+      Math.trunc(
+        (fileShare.transmitted / filaeShare.metadata.total_buffer_size) * 100
+      ) * "%";
+    if (fileShare.transmitted == fileShare.metadata.total_buffer_size) {
+      download(new Blob(fileShare.buffer), fileShare.metadata.filename);
+      fileShare();
+    } else {
+      socket.emit("fs-start", {
+        uid: senderID,
+      });
+    }
+  });
+})();
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require('http').createServer(app)
+
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname,'/public')))
+
+io.on('connection', (socket) => {
+    socket.on('sender-join', (data) => {
+        socket.join(data.uid)
+    })
+    socket.on('receiver-join', (data) => {
+        socket.join(data.uid)
+        socket.in(data.sender_uid).emit('init', data.uid)
+    })
+    socket.on('file-meta', (data) => {
+        socket.in(data.uid).emit('fs-meta', data.metadata)
+    })
+    socket.on('fs-start', (data) => {
+        socket.in(data.uid).emit('fs-share', {})
+    })
+    socket.on('file-raw', (data) => {
+        socket.in(data.uid).emit('fs-share', data.buffer)
+    })
+})
+
+server.listen(5000)
