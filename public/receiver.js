49b140579298b6fe05b2d222af08fdcb1c3a75dc
Ritesh <riteshkokam@gmail.com>
2022-07-15 15:42:28 +0530

    Fix styling and errors

--- public/receiver.js
+++ public/receiver.js
@@ -9,7 +9,7 @@
   }
 
   document
-    .querySelector("#reciver-start-con-btn")
+    .querySelector("#receiver-start-con-btn")
     .addEventListener("click", function () {
       senderID = document.querySelector("#join-id").value;
       if (senderID.length == 0) {
@@ -17,22 +17,18 @@
       }
       let joinID = generateID();
 
-      socket.emit("reciver-join", {
-        uid: joinID,
+      socket.emit("receiver-join", {
         sender_uid: senderID,
+        uid: joinID,
       });
+      document.querySelector(".join-screen").classList.remove("active");
+      document.querySelector(".fs-screen").classList.add("active");
     });
 
-  socket.on("init", function () {
-    receiverID = uid;
-    document.querySelector(".join-screen").classList.remove("active");
-    document.querySelector(".fs-screen").classList.add("active");
-  });
-
-  let filaeShare = {};
+  let fileShare = {};
 
   socket.on("fs-meta", function (metadata) {
-    fileShare.maetadata = metadata;
+    fileShare.metadata = metadata;
     fileShare.transmitted = 0;
     fileShare.buffer = [];
 
@@ -55,17 +51,16 @@
   socket.on("fs-share", function (buffer) {
     fileShare.buffer.push(buffer);
     fileShare.transmitted += buffer.byteLength;
-    fileShare.progress_node.innerText =
-      Math.trunc(
-        (fileShare.transmitted / filaeShare.metadata.total_buffer_size) * 100
-      ) * "%";
-    if (fileShare.transmitted == fileShare.metadata.total_buffer_size) {
-      download(new Blob(fileShare.buffer), fileShare.metadata.filename);
-      fileShare();
-    } else {
-      socket.emit("fs-start", {
-        uid: senderID,
-      });
-    }
+    // fileShare.progress_node.innerText = Math.trunc((fileShare.transmitted / fileShare.metadata.total_buffer_size) * 100) * "%";
+    fileShare.progress_node.innerText = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100);
+		if(fileShare.transmitted == fileShare.metadata.total_buffer_size){
+			console.log("Download file: ", fileShare);
+			download(new Blob(fileShare.buffer), fileShare.metadata.filename);
+			fileShare = {};
+		} else {
+			socket.emit("fs-start",{
+				uid:senderID
+			});
+		}
   });
 })();
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
-const server = require('http').createServer(app)
+const app = express();
+const server = require("http").createServer(app);
 
-const io = require('socket.io')(server)
+const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname,'/public')))
+app.use(express.static(path.join(__dirname + "/public")));
 
-io.on('connection', (socket) => {
-    socket.on('sender-join', (data) => {
-        socket.join(data.uid)
-    })
-    socket.on('receiver-join', (data) => {
-        socket.join(data.uid)
-        socket.in(data.sender_uid).emit('init', data.uid)
-    })
-    socket.on('file-meta', (data) => {
-        socket.in(data.uid).emit('fs-meta', data.metadata)
-    })
-    socket.on('fs-start', (data) => {
-        socket.in(data.uid).emit('fs-share', {})
-    })
-    socket.on('file-raw', (data) => {
-        socket.in(data.uid).emit('fs-share', data.buffer)
-    })
-})
+io.on("connection", function (socket) {
+  socket.on("sender-join", function (data) {
+    socket.join(data.uid);
+  });
 
-server.listen(5000)
+  socket.on("receiver-join", function (data) {
+    socket.join(data.uid);
+    socket.in(data.sender_uid).emit("init", data.uid);
+  });
+
+  socket.on("file-meta", function (data) {
+    socket.in(data.uid).emit("fs-meta", data.metadata);
+  });
+
+  socket.on("fs-start", function (data) {
+    socket.in(data.uid).emit("fs-share", {});
+  });
+
+  socket.on("file-raw", function (data) {
+    socket.in(data.uid).emit("fs-share", data.buffer);
+  });
+});
+
+server.listen(5000);
