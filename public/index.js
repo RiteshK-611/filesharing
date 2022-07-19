747e39bae510b5e66ab8fd86aa12320a37dc6e19
Ritesh <riteshkokam@gmail.com>
2022-07-19 19:05:49 +0530

    Add Drag 'n' Drop Feature

--- public/index.js
+++ public/index.js
@@ -25,31 +25,110 @@
 	});
 
 	document.querySelector("#file-input").addEventListener("change",function(e){
-		let file = e.target.files[0];
-		if(!file){
-			return;		
-		}
-		let reader = new FileReader();
-		reader.onload = function(e){
-			let buffer = new Uint8Array(reader.result);
 
-			let el = document.createElement("div");
-			el.classList.add("item");
-			el.innerHTML = `
-					<div class="progress">0%</div>
-					<div class="filename">${file.name}</div>
-			`;
+		for (let i=0; i<e.target.files.length; i++) {
+			let file = e.target.files[i];
+			let reader = new FileReader();
 			
-			document.querySelector(".files-list").appendChild(el);
-			shareFile({
-				filename: file.name,
-				total_buffer_size:buffer.length,
-				buffer_size:1024,
-			}, buffer, el.querySelector(".progress"));
+			if(!file){
+				return;		
+			}
+			
+			reader.onload = function(e){
+				let buffer = new Uint8Array(reader.result);
+	
+				let el = document.createElement("div");
+				el.classList.add("item");
+				el.innerHTML = `
+						<div class="progress">0%</div>
+						<div class="filename">${file.name}</div>
+				`;
+				
+				document.querySelector(".files-list").appendChild(el);
+				shareFile({
+					filename: file.name,
+					total_buffer_size:buffer.length,
+					buffer_size:1024,
+				}, buffer, el.querySelector(".progress"));
+			}
+			reader.readAsArrayBuffer(file);
 		}
-		reader.readAsArrayBuffer(file);
+
 	});
 
+	document.querySelector(".file-input").addEventListener("drop", function(ev){
+		console.log('File(s) dropped');
+		ev.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+		
+		if (ev.dataTransfer.items) {
+			// Use DataTransferItemList interface to access the file(s)
+			for (let i = 0; i < ev.dataTransfer.items.length; i++) {
+				// If dropped items aren't files, reject them
+				if (ev.dataTransfer.items[i].kind === 'file') {
+					const file = ev.dataTransfer.items[i].getAsFile();
+					console.log('… file1[' + i + '].name = ' + file.name);
+					console.log(file);
+					if(!file){
+						return;		
+					}
+					let reader = new FileReader();
+					reader.onload = function(e){
+						let buffer = new Uint8Array(reader.result);
+
+						let el = document.createElement("div");
+						el.classList.add("item");
+						el.innerHTML = `
+								<div class="progress">0%</div>
+								<div class="filename">${file.name}</div>
+						`;
+						
+						document.querySelector(".files-list").appendChild(el);
+						shareFile({
+							filename: file.name,
+							total_buffer_size:buffer.length,
+							buffer_size:1024,
+						}, buffer, el.querySelector(".progress"));
+					}
+					reader.readAsArrayBuffer(file);
+				}
+			}
+		} else {
+			// Use DataTransfer interface to access the file(s)
+			for (let i = 0; i < ev.dataTransfer.files.length; i++) {
+				console.log('… file2[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+				const file = ev.dataTransfer.files[i].getAsFile();
+				console.log('… file2[' + i + '].name = ' + file.name);
+				console.log(file);
+				if(!file){
+					return;		
+				}
+				let reader = new FileReader();
+				reader.onload = function(e){
+					let buffer = new Uint8Array(reader.result);
+
+					let el = document.createElement("div");
+					el.classList.add("item");
+					el.innerHTML = `
+							<div class="progress">0%</div>
+							<div class="filename">${file.name}</div>
+					`;
+					
+					document.querySelector(".files-list").appendChild(el);
+					shareFile({
+						filename: file.name,
+						total_buffer_size:buffer.length,
+						buffer_size:1024,
+					}, buffer, el.querySelector(".progress"));
+				}
+				reader.readAsArrayBuffer(file);
+			}
+		}
+	})
+
+	document.querySelector(".file-input").addEventListener("dragover", function(ev){
+		console.log('File(s) in drop zone');
+		ev.preventDefault(); // Prevent default behavior (Prevent file from being opened)
+	})
 
 	function shareFile(metadata,buffer,progress_node){
 		socket.emit("file-meta", {
